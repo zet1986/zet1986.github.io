7cbd93cbe732e4e68ba256194d2f5ea8069bf637
zet1986 <51663390+zet1986@users.noreply.github.com>
2019-07-01 16:36:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+//pi = 3;
+
+console.log(pi / 0);
+
+let persone = {
+    "name": "John",
+    "age": 25,
+    "isMarried": false
+}
+
+console.log(persone["age"]);
+
+let answer = prompt('Are you 18?',"Да");
+
+console.log(answer);
+
+console.log(typeof (answer));
+  
